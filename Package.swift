734d31aefa5c33fbd33109b14b840758fd87167f
Arideno <Arideno@users.noreply.github.com>
2023-11-16 14:23:57 +0000

    Run swift-format

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
         .package(url: "https://github.com/stackotter/swift-macro-toolkit", from: "0.3.1"),
-        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1")
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1"),
     ],
     targets: [
         .macro(
