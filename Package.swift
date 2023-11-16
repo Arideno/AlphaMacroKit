031aacfa61232cd335831b5be6e5982e4e7f1995
Andrii Moisol <andrii.moisol@gmail.com>
2023-11-16 12:19:56 +0200

    Add .spi.yml + remove comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 import CompilerPluginSupport
@@ -8,22 +7,17 @@ let package = Package(
     name: "AlphaMacroKit",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AlphaMacroKit",
             targets: ["AlphaMacroKit"]
         )
     ],
     dependencies: [
-        // Depend on the Swift 5.9 release of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
         .package(url: "https://github.com/stackotter/swift-macro-toolkit", from: "0.3.1"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "AlphaMacroKitMacros",
             dependencies: [
@@ -33,13 +27,10 @@ let package = Package(
             ]
         ),
 
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "AlphaMacroKit", dependencies: ["AlphaMacroKitMacros"]),
 
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "AlphaMacroKitClient", dependencies: ["AlphaMacroKit"]),
 
-        // A test target used to develop the macro implementation.
         .testTarget(
             name: "AlphaMacroKitTests",
             dependencies: [
