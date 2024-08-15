89b0fdc5dcacaa8035f03a8bedce1e10e3c7e188
Andrii Moisol <andrii.moisol@alphanovel.io>
2024-08-15 13:50:52 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
     products: [
         .library(
             name: "AlphaMacroKit",
-            type: .dynamic,
             targets: ["AlphaMacroKit"]
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/stackotter/swift-macro-toolkit", from: "0.3.1"),
-        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "510.0.0"),
+        .package(url: "https://github.com/stackotter/swift-macro-toolkit", branch: "main"),
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.5.2"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
     ],
     targets: [
